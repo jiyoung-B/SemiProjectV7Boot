f4f6d6a245867cd56fe22a7b1aa3c516afd65e95
jiyoung-B <catgirl0313@naver.com>
2023-05-23 12:20:20 +0900

    validation 2 - 컨트롤러에 get 요청시 유효성 검사할 모델 선언

--- src/main/java/catgirl/springboot/semiprojectv7/controller/BoardController.java
+++ src/main/java/catgirl/springboot/semiprojectv7/controller/BoardController.java
@@ -4,6 +4,7 @@ import catgirl.springboot.semiprojectv7.model.Board;
 import catgirl.springboot.semiprojectv7.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -55,7 +56,11 @@ public class BoardController {
     }
 
     @GetMapping("/write")
-    public String write(){
+    public String write(Model m){
+
+        // validation을 위한 첫번째 코드
+        m.addAttribute("board", new Board());
+
         return "board/write";
     }
     @PostMapping("/write")
