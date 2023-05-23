5a6606fba3fcc095eb455d6acd759306b16098b5
jiyoung-B <catgirl0313@naver.com>
2023-05-23 18:21:57 +0900

    회원가입시 회원정보를 위한 모델 클래스 작성 (transient 애노테이션 추가)

--- src/main/java/catgirl/springboot/semiprojectv7/model/Member.java
+++ src/main/java/catgirl/springboot/semiprojectv7/model/Member.java
@@ -2,9 +2,11 @@ package catgirl.springboot.semiprojectv7.model;
 
 import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 import java.util.Date;
 
@@ -20,18 +22,52 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long mbno;
     private String name;
+
     private String jumin1;
+
     private String jumin2;
+    @NotBlank(message = "아이디는 필수 입력항목입니다!!")
+    @Length(min=6, max=16, message = "아이디는 최소 6자 ~ 16자 입니다!!")
     private String userid;
+    @NotBlank(message = "비밀번호는 필수항목입니다!!")
+    @Length(min=6, max=16, message = "비밀번호는 최소6자, 최대16자이어야 합니다!!")
     private String passwd;
     private String zipcode;
+    @NotBlank(message = "기본주소는 필수 입력항목입니다!!")
     private String addr1;
+    @NotBlank(message = "나머지주소는 필수 입력항목입니다!!")
     private String addr2;
     private String email;
     private String phone;
     @CreatedDate
     @Column(insertable = false, updatable = false)
     private LocalDateTime regdate;
+
+    // -----------------
+    @Transient   // 필드 영속성은 지원 x
+    @NotBlank(message = "우편번호는 필수항목입니다!!")
+    private String zip1;
+
+    @Transient
+    @NotBlank(message = "우편번호는 필수항목입니다!!")
+    private String zip2;
+
+    @Transient
+    @NotBlank(message = "이메일은 필수항목입니다!!")
+    private String email1;
+
+    @Transient
+    @NotBlank(message = "이메일은 필수항목입니다!!")
+    private String email2;
+
+    @Transient
+    @NotBlank(message = "전화번호는 필수항목입니다!!")
+    private String tel2;
+
+    @Transient
+    @NotBlank(message = "전화번호는 필수항목입니다!!")
+    private String tel3;
+
 
 
 }
