a285b05f2a309586bcefa601ed233c619af56368
jiyoung-B <catgirl0313@naver.com>
2023-05-23 12:52:19 +0900

    validation 4 - 서버측 유효성 검사 여부에 따라 출력할 뷰페이지 지정

--- src/main/java/catgirl/springboot/semiprojectv7/controller/BoardController.java
+++ src/main/java/catgirl/springboot/semiprojectv7/controller/BoardController.java
@@ -5,12 +5,14 @@ import catgirl.springboot.semiprojectv7.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
 import java.util.Map;
 
 @Controller
@@ -64,11 +66,15 @@ public class BoardController {
         return "board/write";
     }
     @PostMapping("/write")
-    public String writeok(Board bd){
-        String viewPage = "error";
-        if(brdsrv.newBoard(bd)){
-            viewPage = "redirect:/board/list?cpg=1";
-        }
+    public String writeok(@Valid Board board,
+                          BindingResult br){  // 서버가 정상적으로 작동하지 않았을 때 오류가 bindingresult에 쌓인다.
+        String viewPage = "redirect:/board/list?cpg=1";
+
+        if(br.hasErrors()) // 유효성 검사시 오류가 발생하면
+            viewPage = "board/write";
+        else
+            brdsrv.newBoard(board);
+
         return viewPage;
     }
 
