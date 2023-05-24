fdc40fc1b31b9c32bf855f08a159d6357cb824ca
jiyoung-B <catgirl0313@naver.com>
2023-05-24 18:18:56 +0900

    자료실 - pdsattach 모델 구현

---
+++ src/main/java/catgirl/springboot/semiprojectv7/model/PdsAttach.java
@@ -0,0 +1,27 @@
+package catgirl.springboot.semiprojectv7.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "pdsattach")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class PdsAttach {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long pano;
+    private String fname;
+    private String ftype;
+    private String fsize;
+    @Column(insertable = false, updatable = false)
+    private String fdown;
+
+    private Integer pno;
+
+
+}
