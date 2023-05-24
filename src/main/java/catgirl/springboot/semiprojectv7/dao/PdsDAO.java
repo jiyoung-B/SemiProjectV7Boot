ea6e3955553d010b4e8eac6019085293d1c5f6d8
jiyoung-B <catgirl0313@naver.com>
2023-05-24 18:16:11 +0900

    자료실 - DAO 구현

---
+++ src/main/java/catgirl/springboot/semiprojectv7/dao/PdsDAO.java
@@ -0,0 +1,8 @@
+package catgirl.springboot.semiprojectv7.dao;
+
+import catgirl.springboot.semiprojectv7.model.Pds;
+
+public interface PdsDAO {
+
+    int insertPds(Pds pds);
+}
---
+++ src/main/java/catgirl/springboot/semiprojectv7/dao/PdsDAOImpl.java
@@ -0,0 +1,18 @@
+package catgirl.springboot.semiprojectv7.dao;
+
+import catgirl.springboot.semiprojectv7.model.Pds;
+import catgirl.springboot.semiprojectv7.repository.PdsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository("pdsdao")
+public class PdsDAOImpl implements PdsDAO{
+    @Autowired
+    PdsRepository pdsRepository;
+    @Override
+    public int insertPds(Pds pds) {
+        // 제목, 작성자, 본문을 pds테이블에 저장한 뒤
+        // 저장시 생성된 pno를 리턴함 - pdsattach 저장시 사용
+        return Math.toIntExact(pdsRepository.save(pds).getPno());
+    }
+}
