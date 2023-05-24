a9532a3e2714ad9752c8d594497c68bd0fbcb01b
jiyoung-B <catgirl0313@naver.com>
2023-05-24 17:07:25 +0900

    자료실 - pds 모델 구현

---
+++ src/main/java/catgirl/springboot/semiprojectv7/model/Pds.java
@@ -0,0 +1,36 @@
+package catgirl.springboot.semiprojectv7.model;
+
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.security.PrivateKey;
+import java.time.LocalDateTime;
+@Table(name="pds")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Pds {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long pno;
+    @NotBlank(message = "제목은 필수항목입니다!!")
+    private String title;
+    @NotBlank(message = "작성자는 필수항목입니다!!")
+    private String userid;
+    @Column(insertable = false, updatable = false)
+    private int thumbs;
+    @Column(insertable = false, updatable = false)
+    private int views;
+    @NotBlank(message = "본문은 필수항목입니다!!")
+    private String content;
+    private String uuid;
+
+    @CreatedDate
+    @Column(insertable = false, updatable = false)
+    private LocalDateTime regdate;
+}
