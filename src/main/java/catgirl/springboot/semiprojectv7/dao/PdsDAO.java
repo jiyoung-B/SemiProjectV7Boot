83f321a2af1b7fa7a49038f72da0de41b28e5ce8
jiyoung-B <catgirl0313@naver.com>
2023-05-25 11:54:07 +0900

    자료실 - 목록보기 dao 구현

--- src/main/java/catgirl/springboot/semiprojectv7/dao/PdsDAO.java
+++ src/main/java/catgirl/springboot/semiprojectv7/dao/PdsDAO.java
@@ -3,8 +3,12 @@ package catgirl.springboot.semiprojectv7.dao;
 import catgirl.springboot.semiprojectv7.model.Pds;
 import catgirl.springboot.semiprojectv7.model.PdsAttach;
 
+import java.util.Map;
+
 public interface PdsDAO {
 
     int insertPds(Pds pds);
     int insertPdsAttach(PdsAttach pa);
+
+    Map<String, Object> selectPds(int i);
 }
