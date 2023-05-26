44dfab24eba4a7a3a0cad091fa88d55fb4ec1267
jiyoung-B <catgirl0313@naver.com>
2023-05-26 16:13:31 +0900

    자료실 댓글 - 댓글쓰기 repository 구현

--- src/main/java/catgirl/springboot/semiprojectv7/repository/PdsReplyRepository.java
+++ src/main/java/catgirl/springboot/semiprojectv7/repository/PdsReplyRepository.java
@@ -2,7 +2,10 @@ package catgirl.springboot.semiprojectv7.repository;
 
 import catgirl.springboot.semiprojectv7.model.PdsReply;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -10,4 +13,8 @@ public interface PdsReplyRepository extends JpaRepository<PdsReply, Long> {
 
     //@Query("from PdsReply where pno = :pno order by refno asc")
     List<PdsReply> findByPnoOrderByRefnoAscRegdateAsc(int pno);
+    @Modifying
+    @Transactional
+    @Query("update PdsReply set refno = :rpno  where rpno = :rpno")
+    void updateRefno(@Param("rpno")int rpno);
 }
