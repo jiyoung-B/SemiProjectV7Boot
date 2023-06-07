0ec7664896a9b650d654880df878dd1790f3c583
jiyoung-B <catgirl0313@naver.com>
2023-06-07 11:52:47 +0900

    갤러리 - 갤러리 클래스 정의

---
+++ src/main/java/catgirl/springboot/semiprojectv7/model/GalAttach.java
@@ -0,0 +1,23 @@
+package catgirl.springboot.semiprojectv7.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "galattach")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class GalAttach {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long gano;
+    private String fname;
+    private String fsize;
+    private Integer gno;
+
+
+}
