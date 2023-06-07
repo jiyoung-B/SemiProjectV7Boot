2380445097a3fc89bc86e9d971b33750cc30b6d0
jiyoung-B <catgirl0313@naver.com>
2023-06-07 11:53:45 +0900

    갤러리 - 갤러리 컨트롤러 정의 - 새글쓰기

--- src/main/java/catgirl/springboot/semiprojectv7/controller/GalleryController.java
+++ src/main/java/catgirl/springboot/semiprojectv7/controller/GalleryController.java
@@ -1,8 +1,17 @@
 package catgirl.springboot.semiprojectv7.controller;
 
+import catgirl.springboot.semiprojectv7.model.Gallery;
+import catgirl.springboot.semiprojectv7.model.Pds;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/gallery")
@@ -11,6 +20,27 @@ public class GalleryController {
     @GetMapping("/list")
     public String list(){
         return "gallery/list";
+    }
+
+    @GetMapping("/write")
+    public String write(Model m){
+        m.addAttribute("gallery", new Gallery());
+        return "gallery/write";
+    }
+
+    @PostMapping("/write") // 전송된 데이터 처리
+    public String writeok(Gallery gallery,
+                          List<MultipartFile> attachs) throws IOException {
+        String viewPage = "error";
+
+//        Map<String, Object> pinfo = pdssrv.newPds(pds);
+//
+//        if(!attach.isEmpty())   // 첨부파일이 존재한다면
+//            pdssrv.newPdsAttach(attach, pinfo);
+//
+//        viewPage = "redirect:/pds/list";
+
+        return viewPage;
     }
 
 
