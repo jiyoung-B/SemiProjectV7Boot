4131fa7e2c89b6b3880bff02db079964ff4322e3
jiyoung-B <catgirl0313@naver.com>
2023-06-08 09:49:03 +0900

    갤러리 - 새글쓰기 처리 - 컨트롤러

--- src/main/java/catgirl/springboot/semiprojectv7/controller/GalleryController.java
+++ src/main/java/catgirl/springboot/semiprojectv7/controller/GalleryController.java
@@ -2,6 +2,8 @@ package catgirl.springboot.semiprojectv7.controller;
 
 import catgirl.springboot.semiprojectv7.model.Gallery;
 import catgirl.springboot.semiprojectv7.model.Pds;
+import catgirl.springboot.semiprojectv7.service.GalleryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +18,8 @@ import java.util.Map;
 @Controller
 @RequestMapping("/gallery")
 public class GalleryController {
+    @Autowired
+    GalleryService galsrv;
 
     @GetMapping("/list")
     public String list(){
@@ -30,15 +34,16 @@ public class GalleryController {
 
     @PostMapping("/write") // 전송된 데이터 처리
     public String writeok(Gallery gallery,
-                          List<MultipartFile> attachs) throws IOException {
+                          List<MultipartFile> attachs){
         String viewPage = "error";
+        System.out.println("어태취스"+attachs);
 
-//        Map<String, Object> pinfo = pdssrv.newPds(pds);
-//
-//        if(!attach.isEmpty())   // 첨부파일이 존재한다면
-//            pdssrv.newPdsAttach(attach, pinfo);
-//
-//        viewPage = "redirect:/pds/list";
+        if(!attachs.isEmpty()) {   // 첨부파일이 존재한다면
+            Map<String, Object> ginfo = galsrv.newGallery(gallery);
+            galsrv.newGalAttach(attachs, ginfo);
+            viewPage = "redirect:/gallery/list";
+        }
+
 
         return viewPage;
     }
